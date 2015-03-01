e601b8ef0da86f00d93a00d5b460fb08283870bc
shigeru <hironoki3@gmail.com>
2015-03-01 15:39:55 +0900

    first commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	[SerializeField]
+	GameObject target = null;
+	[SerializeField]
+	Vector3 offset = Vector3.zero;
+
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = target.transform.localPosition + offset;
+	}
+}
---
+++ Assets/CarController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+	public float speed = 3.0f;
+	public float rotateSpeed = 3.0f;
+	public float gravity = 20.0f;
+	private Vector3 moveDirection = Vector3.zero;
+	private CharacterController controller;
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//if (controller.isGrounded) {
+			transform.Rotate (0, Input.GetAxis ("Horizontal") * rotateSpeed, 0);
+
+			float curSpeed = speed * Input.GetAxis ("Vertical");
+			controller.SimpleMove (transform.forward * curSpeed);
+		//}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move (moveDirection * Time.deltaTime);
+	}
+}
