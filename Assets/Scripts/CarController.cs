f45c98e43fe7a1b6b3d61e40ffbc109b10de3c02
shigeru <hironoki3@gmail.com>
2015-03-12 19:58:44 +0900

    upgrade unity5

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -7,20 +7,34 @@ public class CarController : MonoBehaviour {
 	public float gravity = 20.0f;
 	private Vector3 moveDirection = Vector3.zero;
 	private CharacterController controller;
+	[SerializeField]
+	private GameObject manager;
+	private RaceManager raceManager;
 	// Use this for initialization
 	void Start () {
 		controller = GetComponent<CharacterController> ();
+		raceManager = manager.GetComponent<RaceManager> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		//if (controller.isGrounded) {
+		if (raceManager.State != RaceStates.go) {
+			return;
+		}
+
+		if (controller.isGrounded) {
 			transform.Rotate (0, Input.GetAxis ("Horizontal") * rotateSpeed, 0);
 
 			float curSpeed = speed * Input.GetAxis ("Vertical");
 			controller.SimpleMove (transform.forward * curSpeed);
-		//}
+		}
 		moveDirection.y -= gravity * Time.deltaTime;
 		controller.Move (moveDirection * Time.deltaTime);
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit collision) {
+		if (collision.gameObject.name == "goal") {
+			raceManager.State = RaceStates.goal;
+		}
 	}
 }
---
+++ Assets/Scripts/RaceManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum RaceStates {
+	ready,
+	go,
+	goal
+}
+
+public class RaceManager : MonoBehaviour {
+	public float timer = 5;
+	public float rap = 0;
+	public RaceStates State { get; set; }
+
+	// Use this for initialization
+	void Start () {
+		State = RaceStates.ready;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (State) {
+		case RaceStates.ready:
+			timer -= Time.deltaTime;
+			if (timer <= 0.0f) {
+				timer = 0;
+				State = RaceStates.go;
+			}
+			break;
+		case RaceStates.go:
+			rap += Time.deltaTime;
+			break;
+		case RaceStates.goal:
+			break;
+		default:
+			break;
+		}
+	}
+	private void OnGUI() {
+		GUILayout.BeginVertical (GUILayout.Width (400));
+		switch (State) {
+		case RaceStates.ready:
+			GUILayout.Box (string.Format ("{0}", System.Math.Ceiling (timer % 60f)), GUILayout.Width (400));
+			break;
+		case RaceStates.go:
+		case RaceStates.goal:
+			GUILayout.Box (System.Math.Round(rap, 2).ToString(), GUILayout.Width (400));
+			break;
+		default:
+			break;
+		}
+	}
+}
